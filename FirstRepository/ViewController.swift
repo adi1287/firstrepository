7c6275672d1c65d4d184ee9fc45147523c49bdbb
Aditya Gupta <adityagupta@Adityas-MacBook-Pro.local>
2017-07-31 21:21:25 -0700

    Initial Commit

---
+++ FirstRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstRepository
+//
+//  Created by Aditya Gupta on 7/31/17.
+//  Copyright © 2017 Aditya Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
